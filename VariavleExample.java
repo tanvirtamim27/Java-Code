8553958f74a633bf154ee641cf68bb3de3815fb2
tanvirtamim27 <tanvirtamim0077@gmail.com>
2025-06-24 21:12:10 +0600

    third commit

---
+++ VariavleExample.java
@@ -0,0 +1,20 @@
+public class VariavleExample {
+    public static void main(String[] args) {
+        String name= "Tanvir Islam Tamim";
+        int age= 23;
+        float wait= 55.57f;
+        char BG= 'B';
+        boolean d= true;
+        
+        System.out.println("My salf = "+name);
+        System.out.println("Age = "+age );
+        System.out.println("Wait = " +wait );
+        System.out.println("Blood Group ="+BG);
+        System.out.println("Are you unmarite = "+d);
+
+        int x=10;
+        int y= 15;
+        System.out.println(y>x);
+
+    }
+}
---
+++ Vartiable.java
@@ -0,0 +1,13 @@
+public class Vartiable {
+    public static void main(String[] args) {
+        int a=5;
+        String ab="Tanvir Islam Tamim";
+        float c= 5.256f;
+        double d= 9.2564255;
+        System.out.println(a);
+        System.out.println("Hello "+ab);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}

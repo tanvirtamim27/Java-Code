ec57c07ae23a220a520930ce1ae21818e18775d5
tanvirtamim27 <tanvirtamim0077@gmail.com>
2025-06-26 13:16:45 +0600

    Add files via upload

---
+++ AllMethod.java
@@ -0,0 +1,20 @@
+public class AllMethod {
+     
+         static void Name(String name, int age){
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+
+    }
+
+     static void Thanks(){
+        System.out.println("Thank you for yor response : ");
+    }
+
+    public static void main(String[] args) {
+
+        Thanks();
+        Name("Tamim", 23);
+    }
+
+    }
+

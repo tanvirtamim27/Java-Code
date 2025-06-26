bb9f76f8ccde3cb13cdfad9f3418f80f983203dc
tanvirtamim27 <tanvirtamim0077@gmail.com>
2025-06-26 12:51:15 +0600

    Add files via upload

---
+++ MathFunction.java
@@ -0,0 +1,9 @@
+public class MathFunction {
+    public static void main(String[] args) {
+        int x=16, y=3;
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.sqrt(x));
+        System.out.println(x>y?"Tamim" : "Tafim") ;
+    }
+}

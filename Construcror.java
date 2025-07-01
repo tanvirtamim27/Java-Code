691fae6651e6134610ab4d788c21c9aa0213adf9
tanvirtamim27 <tanvirtamim0077@gmail.com>
2025-07-01 09:40:39 +0600

    Add files via upload

---
+++ Construcror.java
@@ -0,0 +1,7 @@
+public class Construcror {
+    
+    public static void main(String[] args) {
+         Constructor1 ob= new Constructor1();
+         Constructor1 ob2 = new Constructor1(2025);
+    }
+}
---
+++ Constructor1.java
@@ -0,0 +1,10 @@
+public class Constructor1 {
+    public Constructor1(){
+        System.out.println("This is from contsructor without parameter");
+    }
+
+    public Constructor1(int y){
+        System.out.println("Happy New Year : "+y);
+    }
+    
+}

575527f80cc5c859ced86ad1a9e5fe286c285c11
tanvirtamim27 <tanvirtamim0077@gmail.com>
2025-07-01 11:36:27 +0600

    Add files via upload

---
+++ AcessModifiers.java
@@ -0,0 +1,15 @@
+public class AcessModifiers {
+    static void Name(){
+        System.out.println("Tanvir islam Tamim");
+    }
+        void age(){
+            System.out.println("Age : "+23);
+
+        }
+   public static void main(String[] args) {
+    Name();
+    AcessModifiers ob = new AcessModifiers();
+    ob.age();
+   }
+
+}

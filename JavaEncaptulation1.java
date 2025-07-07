f0edf7e7d2b9b13c5bc999149333e5e41d75752b
tanvirtamim27 <tanvirtamim0077@gmail.com>
2025-07-07 11:05:09 +0600

    Add files via upload

---
+++ JavaEncaptulation1.java
@@ -0,0 +1,10 @@
+public class JavaEncaptulation1 {
+    public static void main(String[] args) {
+         JavaEncaptulation ob = new JavaEncaptulation();
+    ob.setName("Tamim");
+    ob.setAge(23);
+    System.out.println(ob.getName());
+    System.out.println(ob.getAge());
+    }
+    
+}
